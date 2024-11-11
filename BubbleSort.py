937e4b0b712cd9cdfc8f3e3706501da7bdbd0c31
ngotuanviet <ngotuanviet9@gmail.com>
2024-11-11 09:20:26 +0700

    commit

--- BubbleSort.py
+++ BubbleSort.py
@@ -5,15 +5,15 @@ arr = []
 
 def bubbleSort(arr):
     for i in range(n):
-        print(f"Vong lap {i + 1}: {arr}")
         for j in range(0, n - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-
+        print(f"Vong lap {i + 1}: {arr}")
 for i in range(n):
     arr.append(int(input(f"Nhap phan tu thu {i + 1}: ")))
 
 system("cls")
+print("ket qua cua BubbleSort \n")
 print(f"Truoc khi sort: {arr}\n")
 bubbleSort(arr)
 print(f"\nKet qua: {arr}")
--- InsertionsSort.py
+++ InsertionsSort.py
@@ -5,18 +5,19 @@ arr = []
 
 def insertionSort(arr):
     for i in range(1, n):
-        print(f"Vong lap {i}: {arr}")
         key = arr[i]
         j = i - 1
         while j >= 0 and key < arr[j]:
             arr[j + 1] = arr[j]
             j -= 1
         arr[j + 1] = key
+        print(f"Vong lap {i}: {arr}")
 
 for i in range(n):
     arr.append(int(input(f"Nhap phan tu thu {i + 1}: ")))
 
 system("cls")
+print("ket qua cua InsertionsSort \n")
 print(f"Truoc khi sort: {arr}\n")
 insertionSort(arr)
 print(f"\nKet qua: {arr}")
